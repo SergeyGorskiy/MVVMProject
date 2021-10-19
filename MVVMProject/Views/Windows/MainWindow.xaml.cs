4477427cc0830745e16873696b1ccefb08d1f38e
Sergey Nazhestkin <9497020@mail.ru>
2021-10-19 12:27:34 +0300

    Создана инфраструктура проекта.

---
+++ MVVMProject/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace MVVMProject
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
